1f7c66925f7ca38c56913ffab660f360ab28c17a
Heli Tuomonen <heli.tuomonen@cs.helsinki.fi>
2017-12-29 22:29:10 +0200

    Lego2

---
+++ Lego2/src/Lego.java
@@ -0,0 +1,11 @@
+import lejos.nxt.Button;
+
+public class Lego {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+        Button.waitForAnyPress();
+	}
+
+}
